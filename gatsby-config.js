a0cae7eb8c6dff4100cb09224a988692162bf033
Kinga <Ma3511ki>
2021-12-21 12:00:45 +0100

    minimal reproduction

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,22 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+        {
+            resolve: 'gatsby-source-flotiq',
+            options: {
+                authToken: '05aa3620e956e66a1ab2847545c043a9',
+                forceReload: false,
+                includeTypes: [
+                    '_media',
+                    'flotiqBlogAuthor',
+                    'flotiqBlogPost',
+                    'flotiqBlogTag',
+                ],
+                downloadMediaFile: true
+            },
+        },
+        'gatsby-plugin-image',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp'
     ]
 }
